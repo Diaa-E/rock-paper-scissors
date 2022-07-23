fcb49376ae3c6399250409a046168fc08e509b28
Diaa-E <blarb21@yahoo.com>
2022-07-23 11:10:10 +0200

    Write pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+//WHILE round is not 5
+
+//play another round
+//track score
+//go to next round
+
+//ELSE
+//Announce winner and score
+
+//generate random number from 1 to 3 (inclusive) for rock, paper and scissors
+
+//sompare user selection and computer selection
+//announce round winner
