340aad4803eee56c62589dc3de827ead61a4c8a6
Diaa-E <blarb21@yahoo.com>
2022-07-23 11:21:35 +0200

    Add computer selection function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //WHILE round is not 5
 
-//play another round
 //track score
 //go to next round
 
@@ -8,6 +7,19 @@
 //Announce winner and score
 
 //generate random number from 1 to 3 (inclusive) for rock, paper and scissors
+function computerSelection()
+{
+    let selection = Math.floor(Math.random()*3 + 1);
 
+    switch (selection)
+    {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+    }
+}
 //sompare user selection and computer selection
 //announce round winner
